9cba5cc55d0bf2b9d5ea7c9efc783e48f94eba18
KauaCantanhedeSantos <kaua.cantanhede.santos@gmail.com>
2024-02-25 16:14:17 -0300

    day 08

---
+++ day08/src/main.js
@@ -0,0 +1,11 @@
+let alertBox = document.getElementById('alertBox');
+
+
+
+function alertsMessages() {
+  let divElement = document.createElement('div');
+  divElement.classList.add('element');
+  divElement.innerHTML = 'success';
+
+  alertBox.appendChild.apply(divElement);
+}
